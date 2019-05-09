d08277594b7c020c2b678376072a80ec4935f9ac
Edgars Mielavs <edgars.mielavs@visma.com>
2019-05-09 11:44:31 +0300

    Karlis template edits

--- API/Function1.cs
+++ API/Function1.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,18 +6,14 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
 using System.Collections.Generic;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Azure.Documents.Client;
-using System.Linq;
 
 namespace API
 {
 
     public class User
     {
-
         [JsonProperty("id")]
         [JsonRequired]
         public string id { get; set; }
@@ -51,27 +46,6 @@ namespace API
 
     public static class Function1
     {
-
-        
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        }
-
-
         [FunctionName("CosmosDb_GetTodos")]
         public static IActionResult GetTodos(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users")]HttpRequest req,

52e87f43105442be0bc61371a5687aeba46bc632
Edgars Mielavs <edgars.mielavs@visma.com>
2019-05-13 14:20:39 +0300

    Adding new project

---
+++ UserWeb2/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UsersWeb.Pages
+{
+    public class Index1Model : PageModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public void OnPost()
+        {
+        }
+        public class InputModel
+        {
+            [Display(Name = "First Name")]
+            public string firstName { get; set; }
+            [Display(Name = "Last Name")]
+            public string lastName { get; set; }
+            [Display(Name = "Id")]
+            public string id { get; set; }
+            [Display(Name = "managerId")]
+            public string managerId { get; set; }
+            [Display(Name = "position")]
+            public string position { get; set; }
+            [Display(Name = "hireDate")]
+            public string hireDate { get; set; }
+            [Display(Name = "dischargeDate")]
+            public string dischargeDate { get; set; }
+        }
+    }
+}
---
+++ UserWeb2/wwwroot/js/JavaScript.js
@@ -0,0 +1,38 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+var HttpClient = function () {
+    this.get = function (aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function () {
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+        anHttpRequest.open("GET", aUrl, true);
+        anHttpRequest.send(null);
+    }
+}
+var theurl = 'https://usersfunc.azurewebsites.net/api/users';
+var client = new HttpClient();
+client.get(theurl, function (response) {
+    var response1 = JSON.parse(response);
+    // alert(response);
+    var table = '<tr><th>Name</th><th>Surname</th><th>ID</th><th>Manager ID</th><th>Position</th><th>Hire date</th><th>Discharge Date</th></tr>';
+    for (r = 0; r < 1; r++) {
+
+        for (var c = 0; c < response1.length; c++) {
+            table += '<tr>';
+            table += '<td>' + response1[c].firstName + '</td>';
+            table += '<td>' + response1[c].lastName + '</td>';
+            table += '<td><a href="https://usersfunc.azurewebsites.net/api/users/' + response1[c].id + '">' + response1[c].id + '</a></td>';
+            table += '<td>' + response1[c].managerId + '</td>';
+            table += '<td>' + response1[c].position + '</td>';
+            table += '<td>' + response1[c].hireDate + '</td>';
+            table += '<td>' + response1[c].dischargeDate + '</td>';
+            table += '</tr>'
+        }
+
+    }
+    document.write('<table style="width:100% display: table; border-collapse: separate; border-spacing: 2px; border-color: gray;">' + table + '</table>')
+});
